368faf376cb4ed72eebf30896d55b8f55fc9e13b
zmharek <zmharek@sqli.com>
2020-11-12 03:06:57 +0100

    Initialize the landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="Container">
+        <h2>DU 09 NOVEMBRE AU 21 NOVEMBRE 2020</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Jouez pour tenter de gagner des cartes cadeaux AMAZON , LIDL , AUCHAN
+          , ACTION , ZALANDO ... valeur de 100€~500€
+        </p>
+        <p>
+          Cerise sur le gâteau : vous serez automatiquement inscrit à notre
+          grand tirage au sort final pour remporter IPHONE 12 Chez .... !
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="App-button"
+          href="http://bit.ly/3lonZas"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          JE JOUE
         </a>
-      </header>
+      </div>
     </div>
   );
 }
